8f7fb5a57a31bec2c97eadc808bd43978f00e8ec
ElitoGame <elitogame123@gmail.com>
2020-07-04 13:07:13 +0200

    Update getbazaardata.js

--- bazaarupdater/getbazaardata.js
+++ bazaarupdater/getbazaardata.js
@@ -1,6 +1,6 @@
-const fetch = require('node-fetch');
-const Sequelize = require('sequelize');
-const { cachedDataVersionTag } = require('v8');
+import fetch from 'node-fetch';
+import Sequelize, { STRING, FLOAT, INTEGER } from 'sequelize';
+import { cachedDataVersionTag } from 'v8';
 
 const sequelize = new Sequelize('database', 'user', 'password', {
 	host: 'localhost',
@@ -12,22 +12,22 @@ const sequelize = new Sequelize('database', 'user', 'password', {
 
 const Monday = sequelize.define('monday', {
 	id: {
-		type: Sequelize.STRING,
+		type: STRING,
 		unique: true,
 		primaryKey: true
 	},
 	min: {
-        type: Sequelize.FLOAT,
+        type: FLOAT,
         defaultValue: 0.0,
         allowNull: false
     },
 	max: {
-        type: Sequelize.FLOAT,
+        type: FLOAT,
         defaultValue: 0.0,
         allowNull: false
     },
 	date: {
-		type: Sequelize.INTEGER,
+		type: INTEGER,
 		defaultValue: 0,
 		allowNull: false,
     }
@@ -35,22 +35,22 @@ const Monday = sequelize.define('monday', {
 
 const Tuesday = sequelize.define('tuesday', {
 	id: {
-		type: Sequelize.STRING,
+		type: STRING,
 		unique: true,
 		primaryKey: true
 	},
 	min: {
-        type: Sequelize.FLOAT,
+        type: FLOAT,
         defaultValue: 0.0,
         allowNull: false
     },
 	max: {
-        type: Sequelize.FLOAT,
+        type: FLOAT,
         defaultValue: 0.0,
         allowNull: false
     },
 	date: {
-		type: Sequelize.INTEGER,
+		type: INTEGER,
 		defaultValue: 0,
 		allowNull: false,
     }
@@ -58,22 +58,22 @@ const Tuesday = sequelize.define('tuesday', {
 
 const Wednesday = sequelize.define('wednesday', {
 	id: {
-		type: Sequelize.STRING,
+		type: STRING,
 		unique: true,
 		primaryKey: true
 	},
 	min: {
-        type: Sequelize.FLOAT,
+        type: FLOAT,
         defaultValue: 0.0,
         allowNull: false
     },
 	max: {
-        type: Sequelize.FLOAT,
+        type: FLOAT,
         defaultValue: 0.0,
         allowNull: false
     },
 	date: {
-		type: Sequelize.INTEGER,
+		type: INTEGER,
 		defaultValue: 0,
 		allowNull: false,
     }
@@ -81,22 +81,22 @@ const Wednesday = sequelize.define('wednesday', {
 
 const Thursday = sequelize.define('thursday', {
 	id: {
-		type: Sequelize.STRING,
+		type: STRING,
 		unique: true,
 		primaryKey: true
 	},
 	min: {
-        type: Sequelize.FLOAT,
+        type: FLOAT,
         defaultValue: 0.0,
         allowNull: false
     },
 	max: {
-        type: Sequelize.FLOAT,
+        type: FLOAT,
         defaultValue: 0.0,
         allowNull: false
     },
 	date: {
-		type: Sequelize.INTEGER,
+		type: INTEGER,
 		defaultValue: 0,
 		allowNull: false,
     }
@@ -104,22 +104,22 @@ const Thursday = sequelize.define('thursday', {
 
 const Friday = sequelize.define('friday', {
 	id: {
-		type: Sequelize.STRING,
+		type: STRING,
 		unique: true,
 		primaryKey: true
 	},
 	min: {
-        type: Sequelize.FLOAT,
+        type: FLOAT,
         defaultValue: 0.0,
         allowNull: false
     },
 	max: {
-        type: Sequelize.FLOAT,
+        type: FLOAT,
         defaultValue: 0.0,
         allowNull: false
     },
 	date: {
-		type: Sequelize.INTEGER,
+		type: INTEGER,
 		defaultValue: 0,
 		allowNull: false,
     }
@@ -127,22 +127,22 @@ const Friday = sequelize.define('friday', {
 
 const Saturday = sequelize.define('saturday', {
 	id: {
-		type: Sequelize.STRING,
+		type: STRING,
 		unique: true,
 		primaryKey: true
 	},
 	min: {
-        type: Sequelize.FLOAT,
+        type: FLOAT,
         defaultValue: 0.0,
         allowNull: false
     },
 	max: {
-        type: Sequelize.FLOAT,
+        type: FLOAT,
         defaultValue: 0.0,
         allowNull: false
 	},
 	date: {
-		type: Sequelize.INTEGER,
+		type: INTEGER,
 		defaultValue: 0,
 		allowNull: false,
     }
@@ -150,139 +150,141 @@ const Saturday = sequelize.define('saturday', {
 
 const Sunday = sequelize.define('sunday', {
 	id: {
-		type: Sequelize.STRING,
+		type: STRING,
 		unique: true,
 		primaryKey: true
 	},
 	min: {
-        type: Sequelize.FLOAT,
+        type: FLOAT,
         defaultValue: 0.0,
         allowNull: false
     },
 	max: {
-        type: Sequelize.FLOAT,
+        type: FLOAT,
         defaultValue: 0.0,
         allowNull: false
     },
 	date: {
-		type: Sequelize.INTEGER,
+		type: INTEGER,
 		defaultValue: 0,
 		allowNull: false,
     }
 });
 
 
-module.exports = {
-    name: 'bazaarupdate',
-    description: 'Saves the current bazaar data',
-    async execute() {
-		const {products} = await fetch('https://api.hypixel.net/skyblock/bazaar?key=dd62008c-45c6-47c8-9575-aa8f1ca5928b').then(response => response.json());
+export const name = 'bazaarupdate';
+export const description = 'Saves the current bazaar data';
+export async function execute() {
+	const { products } = await fetch('https://api.hypixel.net/skyblock/bazaar?key=dd62008c-45c6-47c8-9575-aa8f1ca5928b').then(response => response.json());
 
-		if (!products) {
+	if (!products) {
 		console.log("Error: API not found!");
 		return;
-		}
-
-		
-				
-		var ids = [];
-
-		for (i in products) {
-			//true = items added. //false = already exists or error. with ! means if true -> exists or error!
-			let id = products[i].product_id;
-			if (products[i].sell_summary["0"] == null || products[i].buy_summary["0"] == null) continue;
-			let min = products[i].sell_summary["0"].pricePerUnit;
-			let max = products[i].buy_summary["0"].pricePerUnit;
-			try {
-				if(! await additem(id, min, max)) {
-					edititem(id, min, max)
-				}
-			} catch (e) {}
-
-			ids.push(id);
-			console.log("added: " + id);
-		}
-
-		var app = angular.module('ng_bazaar_app', []);
-			app.controller('ng_bazaar_ctrl', function($scope) {
-				$scope.itemlist = ids;
-		});
-		//console.log('Updated!');
-		return;
-    },
-    async initialize() {
-        Monday.sync();
-        Tuesday.sync();
-        Wednesday.sync();
-        Thursday.sync();
-        Friday.sync();
-        Saturday.sync();
-        Sunday.sync();
-		console.log('Database synced!');
-		
-		const {products} = await fetch('https://api.hypixel.net/skyblock/bazaar?key=dd62008c-45c6-47c8-9575-aa8f1ca5928b').then(response => response.json());
-		const Table = getCurrentTable();
-		if (Table == null) return;
-		
-		if (!products) {
-			console.log("Error: API not found!");
-			return;
-		}
-
-		let date = new Date().getDate;
-		for (i in products) {
-			let id = products[i].product_id;
-			product = await Table.findOne({where: {id: id}});
-			if (product == null) continue;
-			if (product.get('date') != date) {
-				Table.update({date: date}, {where: {id: id}});
-				Table.update({min: 90071992},{where: {id: id}});
-				Table.update({max: 0},{where: {id: id}});
-			}
-		}
-		console.log("Dates adjusted!");
-	},
-	getTable(n) {//Returns a table based on a given weekday and throws a error.
-		let Table;
-		switch (n) {
-			case 1: {
-				Table = Monday;
-				break;
-			}
-			case 2: {
-				Table = Tuesday;
-				break;
-			}
-			case 3: {
-				Table = Wednesday;
-				break;
-			}
-			case 4: {
-				Table = Thursday;
-				break;
-			}
-			case 5: {
-				Table = Friday;
-				break;
-			}
-			case 6: {
-				Table = Saturday;
-				break;
-			}
-			case 7: {
-				Table = Sunday;
-				break;
-			}
-			default: {
-				Table = null;
-			}
-		}
-		if (Table == null) {
-			console.log('An error has accured. Check if the .getDay() works!')
-			return null;
-		}
-		return Table;
 	}
+
+
+
+	var ids = [];
+
+	for (i in products) {
+		//true = items added. //false = already exists or error. with ! means if true -> exists or error!
+		let id = products[i].product_id;
+		if (products[i].sell_summary["0"] == null || products[i].buy_summary["0"] == null)
+			continue;
+		let min = products[i].sell_summary["0"].pricePerUnit;
+		let max = products[i].buy_summary["0"].pricePerUnit;
+		try {
+			if (!await additem(id, min, max)) {
+				edititem(id, min, max);
+			}
+		}
+		catch (e) { }
+
+		ids.push(id);
+		console.log("added: " + id);
+	}
+
+	var app = angular.module('ng_bazaar_app', []);
+	app.controller('ng_bazaar_ctrl', function ($scope) {
+		$scope.itemlist = ids;
+	});
+	//console.log('Updated!');
+	return;
+}
+export async function initialize() {
+	Monday.sync();
+	Tuesday.sync();
+	Wednesday.sync();
+	Thursday.sync();
+	Friday.sync();
+	Saturday.sync();
+	Sunday.sync();
+	console.log('Database synced!');
+
+	const { products } = await fetch('https://api.hypixel.net/skyblock/bazaar?key=dd62008c-45c6-47c8-9575-aa8f1ca5928b').then(response => response.json());
+	const Table = getCurrentTable();
+	if (Table == null)
+		return;
+
+	if (!products) {
+		console.log("Error: API not found!");
+		return;
+	}
+
+	let date = new Date().getDate;
+	for (i in products) {
+		let id = products[i].product_id;
+		product = await Table.findOne({ where: { id: id } });
+		if (product == null)
+			continue;
+		if (product.get('date') != date) {
+			Table.update({ date: date }, { where: { id: id } });
+			Table.update({ min: 90071992 }, { where: { id: id } });
+			Table.update({ max: 0 }, { where: { id: id } });
+		}
+	}
+	console.log("Dates adjusted!");
+}
+export function getTable(n) {
+	let Table;
+	switch (n) {
+		case 1: {
+			Table = Monday;
+			break;
+		}
+		case 2: {
+			Table = Tuesday;
+			break;
+		}
+		case 3: {
+			Table = Wednesday;
+			break;
+		}
+		case 4: {
+			Table = Thursday;
+			break;
+		}
+		case 5: {
+			Table = Friday;
+			break;
+		}
+		case 6: {
+			Table = Saturday;
+			break;
+		}
+		case 7: {
+			Table = Sunday;
+			break;
+		}
+		default: {
+			Table = null;
+		}
+	}
+	if (Table == null) {
+		console.log('An error has accured. Check if the .getDay() works!');
+		return null;
+	}
+	return Table;
 }
 
 async function additem(id, min, max) {
