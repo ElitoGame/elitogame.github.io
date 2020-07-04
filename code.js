93616a5a1ec42e147a6d26a48b904c07d39dfcce
ElitoGame <elitogame123@gmail.com>
2020-07-04 11:17:35 +0200

    css & angular tests

--- code.js
+++ code.js
@@ -1,8 +1,7 @@
 
 var app = angular.module('ng_bazaar_app', []);
 app.controller('ng_bazaar_ctrl', function($scope) {
-    $scope.firstName= "John";
-    $scope.lastName= "Doe";
+    $scope.itemlist.push('melon');
 });
 
 window.onready = function(){
