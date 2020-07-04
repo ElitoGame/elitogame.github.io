b8c6cbfda1a23541bbfd0abc403f1b8ba55cd1de
ElitoGame <elitogame123@gmail.com>
2020-07-04 13:05:01 +0200

    tests

--- bazaarupdater/getbazaardata.js
+++ bazaarupdater/getbazaardata.js
@@ -203,10 +203,10 @@ module.exports = {
 			console.log("added: " + id);
 		}
 
-		/*var app = angular.module('ng_bazaar_app', []);
+		var app = angular.module('ng_bazaar_app', []);
 			app.controller('ng_bazaar_ctrl', function($scope) {
 				$scope.itemlist = ids;
-		});*/
+		});
 		//console.log('Updated!');
 		return;
     },
