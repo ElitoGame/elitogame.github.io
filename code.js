40cc5c122962b7562de63488d9ac30e5700fbc21
ElitoGame <elitogame123@gmail.com>
2020-07-04 11:26:36 +0200

    angular-t

--- code.js
+++ code.js
@@ -1,11 +1,11 @@
 
 var app = angular.module('ng_bazaar_app', []);
 app.controller('ng_bazaar_ctrl', function($scope) {
-    $scope.itemlist.push('melon');
+    $scope.itemlist = ["stone","block","melon"];
 });
 
 window.onready = function(){
     const updater = require(`./bazaarupdater/getbazaardata.js`);
-	updater.initialize();
-	setInterval(function runupdate() {updater.execute().catch()}, 30 * 1000); //for 30 seconds.
+    updater.initialize();
+	  setInterval(function runupdate() {updater.execute().catch()}, 30 * 1000); //for 30 seconds.
 }
