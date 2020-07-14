6d3894f084ed4dc458db2b272f7f5c528a8c2b9a
ElitoGame <elitogame123@gmail.com>
2020-07-14 18:49:48 +0200

    Update code.js

--- code.js
+++ code.js
@@ -2,5 +2,6 @@
 window.onready = function(){
     const updater = require(`./bazaarupdater/getbazaardata.js`);
     updater.initialize();
-	  setInterval(function runupdate() {updater.execute().catch()}, 30 * 1000); //for 30 seconds.
+      setInterval(function runupdate() {updater.execute().catch()}, 30 * 1000); //for 30 seconds.
+    alert(process.env.password);
 }
