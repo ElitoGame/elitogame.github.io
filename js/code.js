4ffcccacbf198ef877ae19dec2fac83682032868
ElitoGame <elitogame123@gmail.com>
2021-01-31 22:03:41 +0100

    Removed loading animations
    - Removed loading animations as they weren't showing up long enough, the .load() and .ready() event ain't precice enough & because the loading icon is disrupting the user experience while switching between images.
    - Removed the image scaleing code as the new system has been implemented a while ago.

--- js/code.js
+++ js/code.js
@@ -9,8 +9,6 @@ On ready-event IMPORTANT!
 And resize event
 *********/
 $(document).ready(function () {
-  // saveImgScales();
-  // scaleImgHeight();
   initHamburger();
   modifyPlaceholders();
   setupSlides();
@@ -27,88 +25,6 @@ window.onresize = function () {
   } else if (isGallerySingleOpen()) {
       $('#cover-background-gallery').height($('#gallery-single-container').get(0).scrollHeight);
   }
-}
-
-/*********
-Image Scaling: 
-*********/
-function saveImgScales() {
-  $('#gallery').children().each(function () {
-    gallery.set(this, {
-      width: $(this).width(),
-      height: $(this).height()
-    });
-  });
-}
-
-function scaleImgHeight() {
-  let size = $('#gallery').width();
-  let sizet = 0;
-  let scale = [];
-  $('#gallery').children().each(function () {
-    let width = gallery.get(this).width;
-    sizet += width;
-    // console.log("i: " + sizet);
-    if (sizet >= size) {
-      // console.log(sizet);
-      sizet = width;
-      updateScale(scale);
-      scale = [];
-    }
-    scale.push(this);
-  });
-  updateScale(scale);
-}
-
-function updateScale(scale) {
-  let width = 0; // width of all unscaled images in one row
-  scale.forEach(element => {
-    let wid = gallery.get(element).width;
-    width += wid;
-  });
-  if (scale.length == 0) return;
-  let height = gallery.get(scale[0]).height; //Height of the individual images
-  let gal_width = $('#gallery').width();
-  let ratio = height / width; // ratio of all the images together to the height
-  let gal_ratio = height / gal_width; // ratio of the gallery
-  let new_height = Math.floor(gal_width * ratio); // the updated height
-
-  // console.log("Elements: " + scale.length + ", Width: " + width + ", Height: " + height + ", Ratio: " + ratio + ", GalRatio: " + gal_ratio + ", GalWidth: " + gal_width + ", NewHeight: " + new_height);
-
-  let total = 0;
-  scale.forEach(element => {
-    let old_width = gallery.get(element).width; // the width of each individual element
-    let ind_ratio = old_width / height; // the ratio each element has
-
-    let new_width = Math.floor(ind_ratio * new_height);
-
-    $(element).width(new_width);
-    $(element).height(new_height);
-    total += new_width;
-  });
-  // console.log("1: " + total + ", compare: " + (total == gal_width));
-  if (total != gal_width) {
-    let difference = parseFloat((gal_width - total).toFixed(2));
-    let adjust = difference / scale.length;
-    // console.log("diff: " + difference);
-    total = 0;
-    scale.forEach(element => {
-      let new_width = $(element).width() + adjust;
-      $(element).width(new_width);
-      total += new_width;
-    });
-    // console.log("2: " + total + ", compare: " + (total == gal_width));
-  }
-}
-
-function getHeight(e) {
-  let h = $(e).css('height');
-  return parseInt(h.slice(0, h.length - 2));
-}
-
-function getWidth(e) {
-  let w = $(e).css('width');
-  return parseInt(w.slice(0, w.length - 2));
 }
 
 /*********
@@ -298,10 +214,6 @@ function opengallerySingle(isVideo, src) {
   // console.log(id)
   container.append(`<h3 class=gallery-single-title>
     ${$(`#gallery-card-${id} .gallery-card-title`).get(0).innerHTML}</h3>`);
-  container.append('<div class="lds-dual-ring"></div>');
-  $('.gallery-single-video').ready(function () {
-    $('.lds-dual-ring').remove();
-  });
 }
 
 function isGallerySingleOpen() {
