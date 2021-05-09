1311638a02ba4c8f639b1abce4cb9ce7e04dd9e2
ElitoGame <elitogame123@gmail.com>
2021-05-09 22:12:34 +0200

    WeekPlanner Update! (Cache, sortable list, jquery!)
    - Added 3 jquery files - slowing down first load, but faster next time
    - Cached static files (jqueryx3, icon, css, code.js)
    - Built a Prototype for the Menu using jquery-ui sortables.
    
    - changed global lets to var
    - updated icons
    - Did some styling with css

--- WeekPlanner/js/code.js
+++ WeekPlanner/js/code.js
@@ -1,6 +1,6 @@
 const weekdays = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday", "Sunday"];
 const day_secs = 86400000;
-let week = [
+var week = [
     {id:0, day:getNextDay(0), isToday:true},
     {id:1, day:getNextDay(1)},
     {id:2, day:getNextDay(2)},
@@ -9,6 +9,8 @@ let week = [
     {id:5, day:getNextDay(5)},
     {id:6, day:getNextDay(6)}
 ];
+var prev_y = 0;
+
 window.onload = () => {
     console.log("Loadin forever more fun ^^");
     
@@ -17,6 +19,17 @@ window.onload = () => {
         if (day.isToday) 
         $('#day_' + (day.id + 1) + '_title').after('<span>(Today)</span>')
     }
+
+    $('#day_1_tasks, #day_2_tasks, #day_3_tasks, #day_4_tasks, #day_5_tasks, #day_6_tasks, #day_7_tasks').sortable({
+        connectWith: '.task_container',
+        tolerance: 'pointer',
+        items: 'li',
+        axis: 'y',
+        handle: '.task_handl',
+        stop: (evt, ui) => {
+            ui.item.removeAttr('style');
+        }
+    })
 }
 
 function getNextDay(add) {
@@ -26,4 +39,8 @@ function getNextDay(add) {
 
 function addTask() {
     console.log("Adding fun!");
+}
+
+function toMainMenu() {
+    console.log("Returning to menu!");
 }
