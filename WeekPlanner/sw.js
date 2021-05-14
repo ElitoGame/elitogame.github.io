62c1853ed0a6c6f8349009e63a78657dd33e4b13
ElitoGame <elitogame123@gmail.com>
2021-05-14 16:52:05 +0200

    Container overhaul | Checkboxes & Deletion | Data storage
    #Containers#
    - The container will now be entirely loaded via JS.
    - Made The Tasks editable
    - Increased size of the Sortable Handle for easier gripping
    #Checkboxs | Deletion#
    - Added Checkboxes
    - When at least 1 checkbox is clicked, a delete button will appear
    #Data storage#
    - Added Keyval idb script
    - implemented storage for most current values.
    #Bugs#
    - Loading tasks causes them to be moved to a wrong container if they have not been moved upon creation
    - Loading tasks will cause reshuffeling per container as the first inserted element is loaded first and placed on top automatically (etc.) - it's keeping the original order (fix ideas via new helper class: array_move

--- WeekPlanner/sw.js
+++ WeekPlanner/sw.js
@@ -2,7 +2,7 @@ const cacheVersion = 'v1';
 importScripts('https://storage.googleapis.com/workbox-cdn/releases/6.1.5/workbox-sw.js');
 const urlsToPrefetch = ['/WeekPlanner/', '/WeekPlanner/js/jquery-3.5.1.min.js',
    '/WeekPlanner/js/code.js', '/WeekPlanner/js/jquery-ui.min.js', '/WeekPlanner/js/jquery.ui.touch-punch.min.js',
-   '/WeekPlanner/css/style.css', '/icons/apple-icon-180.png']
+   '/WeekPlanner/js/idb.min.js', '/WeekPlanner/css/style.css', '/icons/apple-icon-180.png']
 
 workbox.routing.registerRoute(
     ({request}) => request.destination === 'image',
