95a58a69b4a1e6931419f46ce06e41528207bd53
ElitoGame <elitogame123@gmail.com>
2023-08-01 11:23:52 +0200

    meta improvements & 404 page
    - Improved the site description of my portfolio
    - Added a meta image for links
    - Added a 404 page

--- src/consts.ts
+++ src/consts.ts
@@ -2,4 +2,4 @@
 // You can import this data from anywhere in your site by using the `import` keyword.
 
 export const SITE_TITLE = "ElitoGame's Portfolio";
-export const SITE_DESCRIPTION = "Welcome to my portfolio!";
+export const SITE_DESCRIPTION = "Welcome to Elito's portfolio! Here you can find all of my public projects and contact me.";
